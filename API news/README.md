46d35308e529d32559e3b6db453d42fe9fc00487
Carlos Agamez <carlos.agamez@sigma3ds.com>
2024-02-28 16:35:43 -0500

    creacion de modelos

--- API news/README.md
+++ API news/README.md
@@ -1,6 +1,11 @@
 # Api News
 Api de gestion de noticias por medio de RSS 
 
-
 ## Diagrama ER
 ![](../imgs/Diagrama_ER%20.png)
+lis nombres de las tablas y atributos son tal cual los de la base de datos
+
+## Requerimientos
+- ***Obtencion de noticias:*** obten las noticias de los RSS y guardalas en la tabla news (***Trata de q esta ruta sea automatica cada 24horas***).
+- ***Obtencion de noticias por filtro:*** busca noticias por categoria, medio de comunicacion  o fecha de publicion.
+- ***Eliminacion de noticias antoguas:*** elimina las noticas con por lo menos 3 meses de antiguedad en su fecha de publicacion 
--- FKDetector/main/models.py
+++ FKDetector/main/models.py
@@ -1,9 +1,45 @@
 from django.db import models
 
-class Medio(models.Model):
-    name = models.CharField( max_length=50)
+class Media(models.Model):
+    name = models.CharField(max_length=50)
     description = models.TextField()
-    rss_url = models.URLField(blank=True)
+    country = models.CharField(max_length=50)
+    web =  models.URLField(max_length=200, blank=True)
+    logo = models.URLField(max_length=200, blank=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
+
+class Rss_url(models.Model):
+    CATEGORY_CHOICES = [
+        ('politica', 'dolítica'),
+        ('deporte', 'deporte'),
+        ('tecnologia', 'tecnologia'),
+        ('economia', 'economia'),
+        ('entretenimiento', 'entretenimiento'),
+        ('espectaculos', 'espectaculos'),
+        ('salud', 'salud'),
+        ('ciencia', 'ciencia'),
+        ('mundo', 'mundo'),
+        ('cultura', 'cultura'),
+        ('opinion', 'opinion'),
+    ]
+
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    rss = models.URLField()
+    media = models.ForeignKey(Media, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.category} - {self.rss}"
+
+
+class New(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.TextField()
+    body = models.TextField()
+    publication_date = models.DateTimeField()
+    link_article = models.URLField()
+    media = models.ForeignKey(Media, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
