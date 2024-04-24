ca37c4387db0e8fb100cb024f9d991b7232d46d1
Carlos Agamez <carlos.agamez@sigma3ds.com>
2024-04-24 13:37:49 -0500

    diseños del index del cliente listos en un 80%

--- angular-client/src/app/app.routes.ts
+++ angular-client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import {IndexViewComponent} from './index-view/index-view.component'
+import {ValidViewComponent} from './valid-view/valid-view.component'
+export const routes: Routes = [
+    { path: 'index', component: IndexViewComponent },
+    { path: 'validation', component: ValidViewComponent },
+    { path: '', redirectTo: '/index', pathMatch: 'full' } 
+];
